8f6d241188376ce3bd69b6d4abb71bbd20a6cb8a
Rachel Wyatt <rw126@evansville.edu>
2019-04-18 19:25:46 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,18 @@
+let driverId = 0
+let passengerId = 0
+let store = { drivers: [], passengers: [], trips: [] };
+class Driver {
+    constructor(name){
+    this.id = ++driverId;
+    this.name = name;
+    store.drivers.push(this);
+  }
+}
+
+class Passenger {
+  constructor(name) {
+    this.id = ++passengerId;
+    this.name = name ;
+    store.passengers.push(this);
+  }
+}
