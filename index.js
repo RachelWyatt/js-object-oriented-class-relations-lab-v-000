af1bbb23177c98df885ae9f3b123c5ce64a912b6
Rachel Wyatt <rw126@evansville.edu>
2019-04-18 19:31:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ class Driver {
     store.drivers.push(this);
   }
   
-  trips()
+  trips() {
+    return store.trips.filter(
+      function(trip) {
+        return trip.driverId === this.id;
+      }.bind(this) 
+    );
+  }
 }
 
 class Passenger {
